6290422ee5609e37226a893f9efdc18d6be5d7fc
Jason Batchelor <jbatchelor@riversagile.com>
2019-07-29 09:32:44 -0400

    Added mongoose config

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const bodyParser = require('body-parser');
 const game = require('./routes/game.route');
 const app = express();
 
+// Mongoose config
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb+srv://muthacluck:r6g7DA3aZdd9@flustercluck1-1yqpg.mongodb.net/test?retryWrites=true&w=majority';
+let mongoDB = process.env.MONGODB_URL || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connectiondb.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/games', game);
 
 let port = 3004;
