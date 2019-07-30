c27c0fa75f090788d314b8202e36b51d45cb5e62
Jason Batchelor <jbatchelor@riversagile.com>
2019-07-30 10:29:58 -0400

    Added cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 // init the express app
@@ -13,6 +14,17 @@ mongoose.connect(mongoDB, {useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 let db = mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+var whitelist = ['http://localhost:3000'];
+var corsOpts = {
+    origin: function (origin, callback) {
+        if(whitelist.indexOf(origin) != -1){
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
+app.use(cors(corsOpts));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/games', game);
